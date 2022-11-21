ed4bbe2970015dc7cc567671ded82eccccc1f9f6
Xinwei Qiang <qiangxinwei@sjtu.edu.cn>
2022-11-21 12:53:50 +0800

    starting to program

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
+#include <string>
 int main() {
-    int ans = 0;
-    int n = 0;
-    std :: cin >> n;
-    for (int i = 1; i <= n; i++) {
-        ans += i;
-        std :: cout << ans << " ";
-        std :: fflush(stdout);
-    }
-    std :: cout << "hello world!!!";
+    
     return 0;
 }
